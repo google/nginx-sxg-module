39c95bcdcb3daf7fe99f1c04add1e8f233d07dd7
Hiroki Kumazaki <kumagi@google.com>
2020-06-30 06:04:59 +0000

    update e2e test

--- test/selfcheck.sh
+++ test/selfcheck.sh
@@ -7,11 +7,10 @@ fi
 
 rm -rf out
 mkdir out
-curl -i -H"Host:nginx-sxg.test" -H"Accept:application/signed-exchange;v=b3" https://127.0.0.1/bar/ -k --output - > /data/result/index.sxg
-curl -i -H"Host:nginx-sxg.test" -H"Accept:application/signed-exchange;v=b3" https://127.0.0.1/foo/ -k --output - > /data/result/index.html
-curl -i -H"Host:nginx-sxg.test" -H"Accept:application/signed-exchange;v=b3" https://127.0.0.1/bar/ -k --output - | egrep -m1 -B999 '^$'> /data/result/index.header
-curl -H"Host:nginx-no-sxg.test" -H"Accept:application/signed-exchange;v=b3" http://127.0.0.1:8080/ -k --output - > /data/result/http.html
-
+curl -sko- -H"Host:nginx-sxg.test" -H"Accept:application/signed-exchange;v=b3" https://127.0.0.1/bar/ -k --output - > /data/result/index.sxg
+curl -sko- -H"Host:nginx-sxg.test" -H"Accept:application/signed-exchange;v=b3" https://127.0.0.1/foo/ > /data/result/index.html
+curl -siko- -H"Host:nginx-sxg.test" -H"Accept:application/signed-exchange;v=b3" https://127.0.0.1/bar/ | tr -d '\r' | egrep -a -m1 -B999 '^$' > /data/result/index.header
+curl -siko- -H"Host:nginx-no-sxg.test" -H"Accept:application/signed-exchange;v=b3" http://127.0.0.1:8080/ > /data/result/http.html
 
 cp /var/log/nginx/error.log /data/result/
 chmod -R 755 /data/result
@@ -19,3 +18,4 @@ chmod -R 755 /data/result
 cat result/index.sxg
 cat result/error.log
 
+echo "####### selfcheck finished #######"
