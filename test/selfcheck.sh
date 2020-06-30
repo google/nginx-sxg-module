5183507ff72da0ff766f4a675fac1071a3552ce2
Hiroki Kumazaki <kumagi@google.com>
2020-06-30 12:00:20 +0000

    test

--- test/selfcheck.sh
+++ test/selfcheck.sh
@@ -10,7 +10,7 @@ mkdir out
 curl -sko- -H"Host:nginx-sxg.test" -H"Accept:application/signed-exchange;v=b3" https://127.0.0.1/bar/ -k --output - > /data/result/index.sxg
 curl -sko- -H"Host:nginx-sxg.test" -H"Accept:application/signed-exchange;v=b3" https://127.0.0.1/foo/ > /data/result/index.html
 curl -siko- -H"Host:nginx-sxg.test" -H"Accept:application/signed-exchange;v=b3" https://127.0.0.1/bar/ | tr -d '\r' | egrep -a -m1 -B999 '^$' > /data/result/index.header
-curl -siko- -H"Host:nginx-no-sxg.test" -H"Accept:application/signed-exchange;v=b3" http://127.0.0.1:8080/ > /data/result/http.html
+curl -sko- -H"Host:nginx-no-sxg.test" -H"Accept:application/signed-exchange;v=b3" http://127.0.0.1:8080/ > /data/result/http.html
 
 cp /var/log/nginx/error.log /data/result/
 chmod -R 755 /data/result
